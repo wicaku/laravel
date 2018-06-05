09296b1c8e22d150cd5540fce76a28ad10307ae0
Nody Risky Pratomo <nodyriskypratomo@gmail.com>
2018-06-06 00:01:07 +0700

    mysql connection worked

--- config/database.php
+++ config/database.php
@@ -41,17 +41,17 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
+            'host' => env('MYSQL_DB_HOST', '127.0.0.1'),
+            'port' => env('MYSQL_DB_PORT', '3306'),
+            'database' => env('MYSQL_DB_DATABASE', 'egovbench'),
+            'username' => env('MYSQL_DB_USERNAME', 'root'),
+            'password' => env('MYSQL_DB_PASSWORD', ''),
+            // 'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'strict' => false,
-            'engine' => null,
+            // 'prefix' => '',
+            // 'strict' => false,
+            // 'engine' => null,
         ],
 
         'pgsql' => [
